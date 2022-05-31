21a2e3e8e2197ec9ff079a6c78ec4e1f4bca9360
Tjalve Aarflot <tjalve.aarflot@itverket.no>
2022-05-31 21:49:53 +0200

    Deploying the working version of categoryFilter

--- groq/getPageQuery.js
+++ groq/getPageQuery.js
@@ -17,7 +17,7 @@ export function getPageQuery() {
             _type,
             categoryFilter,
             categoryFilterOld,
-            'posts': *[_type == "post" && category._ref in ^.categoryFilter[]._ref]
+            'posts': *[_type == "post" && category._ref == ^.categoryFilter._ref]
             {
                 _id,
                 _type,
