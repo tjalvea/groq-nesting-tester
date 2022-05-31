649c6adca487249d590a048292245af6fd7b0f09
Tjalve Aarflot <tjalve.aarflot@itverket.no>
2022-05-31 17:45:36 +0200

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    theme: {
+      extend: {
+        colors: {
+          teal: colors.teal,
+          cyan: colors.cyan,
+        },
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
